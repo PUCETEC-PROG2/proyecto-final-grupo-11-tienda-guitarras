f01c8fb12cc7d33bc62b6bac3d6afa410d96c003
Ostin22 <rrcarrion@puce.edu.ec>
2024-08-04 18:47:40 -0500

    agregar cliente

--- guitars/urls.py
+++ guitars/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('login/', views.CustomLoginView.as_view(), name="login"),
     path('logout/', LogoutView.as_view(), name="logout"),
+    path('add_client/', views.add_client, name="add_client")
 ]
--- guitars/views.py
+++ guitars/views.py
@@ -1,7 +1,10 @@
 from django.template import loader
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.views import LoginView
+from django.contrib.auth.decorators import login_required
+
+from guitars.forms import ClientForm
 
 
 # Create your views here.
@@ -14,3 +17,15 @@ def index(request):
 #Login
 class CustomLoginView(LoginView):
     template_name = 'login.html'
+    
+    
+@login_required
+def add_client(request):
+    if request.method == 'POST':
+        form = ClientForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect ('guitars:index')
+    else:
+        form = ClientForm()
+    return render(request, 'clientes_form.html', {'form':form})
